2c7f006539b80d05506d4abf1144733f224ef528
Pc da Gubee <gabrielrodriguesdecastro@gmail.com>
2020-09-24 14:36:37 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoImplJBDC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
-        return new SellerDaoImplJBDC();
+        return new SellerDaoImplJBDC(DB.getConnection());
     }
 }
